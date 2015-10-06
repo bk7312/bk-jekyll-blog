65d9d60c0be3119556ccf2058fb2339494930bcc
bk7312 <bk7312@users.noreply.github.com>
2015-10-06 21:01:24 +0800

    $commit_msg

--- gittyup.sh
+++ gittyup.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 # gittyup.sh: a script to automate github stuff...
 
+echo '\n git status before git add\n'
 git status
 
 git add *
 
+echo '\n git status after git add * (adds every changes made)\n'
 git status
 
+echo '\n Enter commit message or ctrl-c to abort:\n'
 read commit_msg
+git commit -m '$commit_msg'
 
-git commit -m "$commit_msg"
-
+echo '\n git status after committing'
 git status
 
-#git push
+echo '\n git push'
+git push
